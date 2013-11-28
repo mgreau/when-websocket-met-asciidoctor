a3500f2a1ec9f5e83e62da1e95b9ea118bd9b028
Maxime Gréau <contact@mgreau.com>
2013-11-29 00:55:05 +0100

    refactor JS files with directives and templates + start authentification process

---
+++ src/main/webapp/js/directives.js
@@ -0,0 +1,73 @@
+app.directive("html5render", function(){
+	return{
+		restrict: 'AE',
+		templateUrl: "templates/html5-render.html",
+		replace: true,
+		scope: false,
+		link: function (scope, element, attrs, ctrl) {
+			scope.theHtml5 = [];
+			scope.theAdocId = "";
+    		scope.$watch(attrs.html5, function(newVal, oldVal) {
+        		scope.theHtml5 = newVal;
+    		});
+
+    		scope.$watch(attrs.key, function(newVal, oldVal) {
+        		scope.theAdocId = newVal;
+    		});
+
+		}
+	};
+});
+
+app.directive("asciidoceditor", function(){
+	return{
+		restrict: 'AE',
+		templateUrl: "templates/asciidoc-editor.html",
+		replace: true,
+		link: function (scope, element, attrs, ctrl) {
+			scope.theAdocId = "";
+
+			scope.$watch(attrs.key, function(newVal, oldVal) {
+        		scope.theAdocId = newVal;
+    		});
+
+		}
+	};
+});
+
+app.directive("collaborativemessage", function(){
+	return{
+		restrict: 'AE',
+		templateUrl: "templates/collaborative-msg.html",
+		replace: true,
+		link: function (scope, element, attrs, ctrl) {
+			scope.theDoc = "";
+
+			scope.$watch(attrs.doc, function(newVal, oldVal) {
+        		scope.theDoc = newVal;
+    		});
+
+		}
+	};
+});
+
+app.directive("auth", function(){
+	return{
+		restrict: 'AE',
+		templateUrl: "templates/auth.html",
+		replace: true,
+		link: function (scope, element, attrs, ctrl) {
+			
+			scope.theNewDoc = "";
+
+			scope.$watch(attrs.doc, function(newVal, oldVal) {
+        		scope.theNewDoc = newVal;
+    		});
+			
+			
+		}
+	};
+});
+
+
+
