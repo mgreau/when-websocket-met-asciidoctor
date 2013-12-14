81c01b9012b51e5dbf0cfa42ae604aa5631944e5
Maxime Gréau <contact@mgreau.com>
2013-12-14 01:00:24 +0100

    add more arquillian tests + fix timeToRender bug
    Former-commit-id: 87ea065d647b1a11b1846603ed5f1918c01a3c86

--- src/test/java/com/mgreau/wwsmad/websocket/WWSMADEndpointTest.java
+++ src/test/java/com/mgreau/wwsmad/websocket/WWSMADEndpointTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import javax.websocket.ContainerProvider;
@@ -24,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.mgreau.wwsmad.StarterService;
+import com.mgreau.wwsmad.websocket.client.MyBasicEndpointClient;
 
 @RunWith(Arquillian.class)
 public class WWSMADEndpointTest {
@@ -41,23 +43,56 @@ public class WWSMADEndpointTest {
 				.addAsManifestResource("MANIFEST.MF")
 				.addAsWebInfResource("beans.xml");
 	}
-	
+
 	@Test
 	@InSequence(1)
 	public void testNotificationOnOpenConnection() throws URISyntaxException,
 			DeploymentException, IOException, InterruptedException {
-		final String JSONNotificationOnOpen = "{\"type\":\"notification\",\"adocId\":\""+ ADOC_ID +"\",\"data\":{\"nbConnected\":1,\"nbWriters\":0,\"writers\":{}}}";
-		Session session = connectToServer(WSEndpointClientTest.class,
-				ADOC_URL+ADOC_ID);
+		// onOpen - notifOnOpen
+		MyBasicEndpointClient.latch = new CountDownLatch(2);
+
+		final String JSONNotificationOnOpen = "{\"type\":\"notification\",\"adocId\":\""
+				+ ADOC_ID
+				+ "\",\"data\":{\"nbConnected\":1,\"nbWriters\":0,\"writers\":{}}}";
+		Session session = connectToServer(MyBasicEndpointClient.class, ADOC_URL
+				+ ADOC_ID);
 		assertNotNull(session);
 
-		assertTrue(WSEndpointClientTest.latch.await(2, TimeUnit.SECONDS));
-		assertEquals(JSONNotificationOnOpen, WSEndpointClientTest.response);
+		try {
+			assertTrue(MyBasicEndpointClient.latch.await(2, TimeUnit.SECONDS));
+			assertEquals(JSONNotificationOnOpen,
+					MyBasicEndpointClient.notificationMessage);
+		} finally {
+			session.close();
+		}
 	}
-	
+
+	@Test
+	@InSequence(2)
+	public void testNotificationWhenBecameAWriter() throws URISyntaxException,
+			DeploymentException, IOException, InterruptedException {
+		final String writer = "@mgreau";
+		final String JSONNotificationWhenBecameAWriter = "{\"type\":\"notification\",\"adocId\":\""
+				+ ADOC_ID
+				+ "\",\"data\":{\"nbConnected\":0,\"nbWriters\":1,\"writers\":{\""
+				+ writer + "\":\"" + writer + "\"}}}";
+
+		// notifOnOpen - notifWhenSend Adoc - output
+		MyBasicEndpointClient.latch = new CountDownLatch(4);
+
+		Session session = connectToServer(MyBasicEndpointClient.class, ADOC_URL
+				+ ADOC_ID);
+		assertNotNull(session);
+
+		session.getBasicRemote().sendText(data);
+		assertTrue(MyBasicEndpointClient.latch.await(5, TimeUnit.SECONDS));
+		assertEquals(JSONNotificationWhenBecameAWriter,
+				MyBasicEndpointClient.notificationMessage);
+	}
+
 	/**
-	 * WebSocket is not yet supported by default by Arquillian, so we need to change the
-	 * schema manually.
+	 * WebSocket is not yet supported by default by Arquillian, so we need to
+	 * change the schema manually.
 	 * 
 	 * @param endpoint
 	 * @param uriPart
@@ -77,4 +112,6 @@ public class WWSMADEndpointTest {
 		System.out.println("Connecting to: " + uri);
 		return container.connectToServer(endpoint, uri);
 	}
+
+	private String data = "{\"type\":\"adoc\",\"source\":\"= Hello Test\\nDoc Writer <doc@example.com>\\nv1.0, 2013-11-11\\n:toc:\\n:numbered:\\n:source-highlighter: coderay\\n\\nAn introduction to http://asciidoc.orgdf[AsciiDoc].\\n\\n\\n\",\"writer\":\"@mgreau\"}";
 }
